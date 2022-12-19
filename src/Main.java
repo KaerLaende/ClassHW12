4bbdf389343503f33bb41df75bcd712e19ad5898
Kaerlaende <kaerlaende@mail.ru>
2022-12-19 18:17:36 +0300

    забыл годы менять)

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,17 @@ public class Main {
         Books warAndPeace = new Books(levTolstoi,"Война и мир", 1867);
         Books crimeAndPunishment = new Books(fedorDostoevskiy, "Преступление и наказание", 1866);
 
+        warAndPeace.setPublishingYear(2022);
+        crimeAndPunishment.setPublishingYear(2022);
+
         Books[] books ={warAndPeace,crimeAndPunishment};
+        for (int i = 0; i < books.length; i++) {
+            System.out.println(books[i]);
+        }
+        System.out.println("Всё х*ня, давай с начало!");
+
+        warAndPeace.setPublishingYear(1867);
+        crimeAndPunishment.setPublishingYear(1866);
         for (int i = 0; i < books.length; i++) {
             System.out.println(books[i]);
         }
