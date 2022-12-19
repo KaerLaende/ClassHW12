d3fc645ba19d7e0a8073aee520654a7b4377bbfa
Kaerlaende <kaerlaende@mail.ru>
2022-12-19 17:23:13 +0300

    Нубас писал код...

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        String authorNameTolstoy = "Л.Н.Толстой";
+        String bookNameTolstoy = "Война и мир";
+        int publishingYearTolstoy = 1867;
 
+        String authorNameDostoevsky = "Ф.М.Достоевский";
+        String bookNameDostoevsky = "Преступление и наказание";
+        int publishingYearDostoevsky = 1866;
         System.out.println("Hello world!");
     }
 }
